58a7ceee17a2e8c4a2d3a8f09f174949d6aaee92
Anvilabs <developers@anvilabs.com>
2025-06-23 21:14:59 +0100

    Initial Commit

---
+++ TrackEd/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TrackEd
+//
+//  Created by Pradeep Reddy Kumbam on 23/06/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TrackEd/TrackEdApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackEdApp.swift
+//  TrackEd
+//
+//  Created by Pradeep Reddy Kumbam on 23/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct TrackEdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
